72d611210429a35edd15efdfeb9e1a7647489f15
Rishit Umesh Padagatti <rishitpadagatti@gmail.com>
2024-12-25 18:16:03 +0530

    9.1

--- Python/8b.py
+++ Python/8b.py
@@ -9,9 +9,6 @@ from bokeh.models import ColumnDataSource
 x = [1,2,3,4,5]
 y = [2,4,6,8,10]
 
-# Create a Bokeh figure
-output_file("8b.html")
-
 # Line Plot
 p1 = figure(title="Line Plot", x_axis_label="X-axis", y_axis_label="Y-axis")
 p1.line(x, y, line_color='blue', line_width=2)
